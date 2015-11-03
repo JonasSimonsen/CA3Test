2c52b090a97881abb2c2931ae0ff5da312fb1de2
jonassimonsen <jonassimonsen@192.168.87.109>
2015-11-03 19:16:43 +0100

    Works with 6 views

--- AngSeedClient/app/view5/view5.js
+++ AngSeedClient/app/view5/view5.js
@@ -4,4 +4,10 @@
  * and open the template in the editor.
  */
 
+angular.module('myApp.view5', ['ngRoute'])
 
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view5', {
+                    templateUrl: 'view5/view5.html'
+                });
+            }]);
--- AngSeedClient/app/view6/view6.js
+++ AngSeedClient/app/view6/view6.js
@@ -4,4 +4,21 @@
  * and open the template in the editor.
  */
 
+angular.module('myApp.view6', ['ngRoute'])
 
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view6', {
+                    templateUrl: 'view6/view6.html',
+                    controller: 'View6Ctrl'
+                });
+            }])
+        .controller('View6Ctrl', function ($scope, $http) {
+            $scope.message = "Create a user to the system!!!";
+            $scope.saveUser = function () {
+//                alert('User');
+                $http.post('api/saveUser', $scope.user).
+                        success(function () {
+                            alert('User Created');
+                          });
+            };
+        });
